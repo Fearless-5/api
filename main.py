7da737a88f104854bc03ebf25807c3a61a2d25f5
Fearless-5 <160542986+Fearless-5@users.noreply.github.com>
2024-09-17 18:36:46 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from faker import Faker
+from countryinfo import CountryInfo
+
+fake = Faker()
+coun = CountryInfo(fake.country())
+app = Flask(__name__)
+@app.route("/")
+def home():
+    return {
+        "name": fake.name(),
+        "url": fake.url(),
+        "country": fake.country(),
+        "email": fake.email(),
+        "country": {
+            "name": coun.name(),
+            "currencies": coun.currencies(),
+            "capital": coun.capital(),
+            "language": coun.languages(),
+            "population": coun.population(),
+        }
+    }
+    
+app.run(debug=True)
